ea9469f0d4b8a55910987ebd7c73584f6f87bdca
Pepe de Sintas Mesquida <josemaria.desintasmesquida@usp.ceu.es>
2025-11-13 19:01:24 +0100

    pepe mucha cosa

--- src/main/java/POJOs/Doctor.java
+++ src/main/java/POJOs/Doctor.java
@@ -9,9 +9,10 @@ public class Doctor extends User {
     private LocalDate dob;
     private Set<Patient> patients;
 
-    public Doctor(String email, String password, String fullName, LocalDate dob) {
+    public Doctor(String email, String password, String fullName, LocalDate dob, Set<Patient> patients) {
         super(email, fullName);
         this.password = password;
         this.dob = dob;
+        this.patients = patients;
     }
 }
--- src/main/java/POJOs/Patient.java
+++ src/main/java/POJOs/Patient.java
@@ -1,6 +1,7 @@
 package POJOs;
 
 import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.Set;
 
 public class Patient extends User {
@@ -9,8 +10,11 @@ public class Patient extends User {
     private LocalDate dob;
     private Set <Report> reports;
 
-    public Patient(String email, String password, String fullName, LocalDate dob) {
+    public Patient(String email, String password, String fullName, LocalDate dob, Doctor doctor, Set<Report> reports) {
         super(email, fullName);
         this.dob = dob;
+        this.password = password;
+        this.reports = reports;
+        this.doctor = doctor;
     }
 }
