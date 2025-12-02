7887adaaa5f94bb8d32f228a0927f9d5d6466bb0
Pepe de Sintas Mesquida <josemaria.desintasmesquida@usp.ceu.es>
2025-12-02 17:47:19 +0100

    pepe  cosa

--- src/main/java/utilities/Utilities.java
+++ src/main/java/utilities/Utilities.java
@@ -15,90 +15,72 @@ public class Utilities {
      */
 
 
-        /**
-         * Reads an integer from the user after displaying a prompt.
-         * Ensures that the input is valid and repeats the prompt if the input is not a valid integer.
-         *
-         * @param question The prompt to display to the user.
-         * @return A valid integer input by the user.
-         */
-        public static int readInteger(String question) {
-            InputStreamReader input = new InputStreamReader(System.in);
-            BufferedReader buffer = new BufferedReader(input);
-            int num;
-            String line;
-            while (true) {
-                try {
-                    System.out.print(question);
-                    line = buffer.readLine();
-                    num = Integer.parseInt(line);
-                    return num;
+    /**
+     * Reads an integer from the user after displaying a prompt.
+     * Ensures that the input is valid and repeats the prompt if the input is not a valid integer.
+     *
+     * @param question The prompt to display to the user.
+     * @return A valid integer input by the user.
+     */
+    public static int readInteger(String question) {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader buffer = new BufferedReader(input);
+        int num;
+        String line;
+        while (true) {
+            try {
+                System.out.print(question);
+                line = buffer.readLine();
+                num = Integer.parseInt(line);
+                return num;
 
-                } catch (IOException ioe) {
-                    System.out.println(" ERROR: Unable to read.");
+            } catch (IOException ioe) {
+                System.out.println(" ERROR: Unable to read.");
 
-                } catch (NumberFormatException nfe) {
-                    System.out.println(" ERROR: Must be a whole number.");
-                }
+            } catch (NumberFormatException nfe) {
+                System.out.println(" ERROR: Must be a whole number.");
             }
         }
+    }
 
-        /**
-         * Reads a string from the user after displaying a prompt.
-         * Ensures that the input is valid and repeats the prompt if an error occurs during reading.
-         *
-         * @param question The prompt to display to the user.
-         * @return A valid string input by the user.
-         */
-        public static String readString(String question) {
-            InputStreamReader input = new InputStreamReader(System.in);
-            BufferedReader buffer = new BufferedReader(input);
-            String line;
-            while (true) {
-                try {
-                    System.out.print(question);
-                    line = buffer.readLine();
-                    return line;
+    /**
+     * Reads a string from the user after displaying a prompt.
+     * Ensures that the input is valid and repeats the prompt if an error occurs during reading.
+     *
+     * @param question The prompt to display to the user.
+     * @return A valid string input by the user.
+     */
+    public static String readString(String question) {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader buffer = new BufferedReader(input);
+        String line;
+        while (true) {
+            try {
+                System.out.print(question);
+                line = buffer.readLine();
+                return line;
 
-                } catch (IOException ioe) {
-                    System.out.println(" ERROR: Unable to read.");
-                }
+            } catch (IOException ioe) {
+                System.out.println(" ERROR: Unable to read.");
             }
         }
-        /**
-         * Validates an email string to ensure it follows a proper email format.
-         *
-         * @param email The email string to validate.
-         * @return {@code true} if the email format is valid; {@code false} otherwise.
-         */
-        public static boolean checkEmail(String email){
-            Pattern pattern = Pattern.compile("([a-z0-9]+(\\.?[a-z0-9])*)+@(([a-z]+)\\.([a-z]+))+");
-            Matcher mather = pattern.matcher(email);
-            if (mather.find()) {
-                return true;
-            } else {
-                System.out.println("-> Please follow the email format: example@example.com");
-                System.out.println("----------------------------------------------");
-                return false;
-            }
-        }
-        /**
-         * Formats a given MAC address string by adding colons (':') after every two characters.
-         *
-         * @param input The unformatted MAC address string.
-         * @return The formatted MAC address string.
-         */
+    }
 
-        /**
-         * Placeholder method for registering patient data.
-         * Currently, this method returns {@code null}.
-         *
-         * @return A {@code Patient} object containing registered data or {@code null}.
-         */
-
-        public static String randomColor(){
-            Random rand = new Random();
-            String[] colorPool = {"\u001B[40m", "\u001B[41m", "\u001B[42m", "\u001B[43m", "\u001B[44m", "\u001B[45m", "\u001B[46m", "\u001B[47m"};
-            return colorPool[rand.nextInt(colorPool.length)];
+    /**
+     * Validates an email string to ensure it follows a proper email format.
+     *
+     * @param email The email string to validate.
+     * @return {@code true} if the email format is valid; {@code false} otherwise.
+     */
+    public static boolean checkEmail(String email) {
+        Pattern pattern = Pattern.compile("([a-z0-9]+(\\.?[a-z0-9])*)+@(([a-z]+)\\.([a-z]+))+");
+        Matcher mather = pattern.matcher(email);
+        if (mather.find()) {
+            return true;
+        } else {
+            System.out.println("-> Please follow the email format: example@example.com");
+            System.out.println("----------------------------------------------");
+            return false;
         }
+    }
 }
